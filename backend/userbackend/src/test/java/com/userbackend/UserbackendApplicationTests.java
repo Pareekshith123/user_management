51735a3be97134bfa8b43c614d98f751d396778f
Pareekshith TG <pareekshithtg@gmail.com>
2023-07-27 11:56:18 +0530

    initial  commit

---
+++ backend/userbackend/src/test/java/com/userbackend/UserbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
